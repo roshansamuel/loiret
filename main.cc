89a2ca29dfbb5fa4e4184598ab0b49b18b7fb2a2
Roshan Samuel <findrjsam@gmail.com>
2020-01-29 10:34:39 +0530

    Corrected bug in initialization. Updated README. More explanatory comments

--- main.cc
+++ main.cc
@@ -6,15 +6,69 @@
 
 /**
  ********************************************************************************************************************************************
- * \brief   Function to impose the Taylor Green Vortex initial condition on the given input velocity field
+ * \brief   Declaration of function to impose a sinusoidal variation for the input plain scalar field.
  *
- *          Depending on the preprocessor flag PLANAR, the function applies the Taylor Green vortex equation
+ ********************************************************************************************************************************************
+ */
+void initializeField(plainsf &uField, grid &mesh);
+
+int main() {
+    // INITIALIZE MPI
+    MPI_Init(NULL, NULL);
+
+    // ALL PROCESSES READ THE INPUT PARAMETERS IN input/parameters.yaml FILE
+    parser inputParams;
+
+    // INITIALIZE PARALLELIZATION DATA
+    parallel mpi(inputParams);
+
+    // INITIALIZE GRID DATA
+    grid gridData(inputParams, mpi);
+
+    // POISSON SOLVER
+    poisson *mgSolver;
+
+    // TEMPLATE SCALAR FIELD (sfield) FOR PRESCRIBING PLAIN SCALAR FIELD (plainsf)
+    sfield P(gridData, "P");
+
+    // PLAIN SCALAR FIELDS THAT SERVE AS LHS AND RHS FOR THE POISSON SOLVER
+    plainsf mgLHS(gridData, P);
+    plainsf mgRHS(gridData, P);
+
+    // INITIALIZE THE RHS WITH A SINUSOIDAL VARIATION
+    initializeField(mgRHS, gridData);
+
+#ifdef PLANAR
+    mgSolver = new multigrid_d2(gridData, inputParams);
+#else
+    mgSolver = new multigrid_d3(gridData, inputParams);
+#endif
+
+    if (mpi.rank == 0 ) std::cout << "Value of LHS at (5, 5, 5) before solving: " << mgLHS.F(5, 5, 5) << std::endl;
+
+    // SOLVE THE POISSON EQUATION
+    mgSolver->mgSolve(mgLHS, mgRHS);
+
+    if (mpi.rank == 0 ) std::cout << "Value of LHS at (5, 5, 5) after solving: " << mgLHS.F(5, 5, 5) << std::endl;
+
+    // FINALIZE AND CLEAN-UP
+    MPI_Finalize();
+
+    return 0;
+}
+
+
+/**
+ ********************************************************************************************************************************************
+ * \brief   Definition of function to impose a sinusoidal variation for the input plain scalar field.
+ *
+ *          Depending on the preprocessor flag PLANAR, the function applies the sinusoidal variation
  *          to set initial conditions in both 2D and 3D cases.
  *
  ********************************************************************************************************************************************
  */
 void initializeField(plainsf &uField, grid &mesh) {
-    if (mesh.rankData.rank == 0) std::cout << "Imposing Taylor-Green vortices initial condition" << std::endl << std::endl;
+    if (mesh.rankData.rank == 0) std::cout << "Imposing sinusoidal initial condition" << std::endl << std::endl;
 
 #ifdef PLANAR
     for (int i=uField.F.lbound(0); i <= uField.F.ubound(0); i++) {
@@ -34,40 +88,4 @@ void initializeField(plainsf &uField, grid &mesh) {
         }
     }
 #endif
-}
-
-
-int main() {
-    // INITIALIZE MPI
-    MPI_Init(NULL, NULL);
-
-    // ALL PROCESSES READ THE INPUT PARAMETERS
-    parser inputParams;
-
-    // INITIALIZE PARALLELIZATION DATA
-    parallel mpi(inputParams);
-
-    // INITIALIZE GRID DATA
-    grid gridData(inputParams, mpi);
-
-    poisson *mgSolver;
-
-    sfield P(gridData, "P");
-    plainsf Pp(gridData, P);
-    plainsf mgRHS(gridData, P);
-
-    initializeField(Pp, gridData);
-
-#ifdef PLANAR
-    mgSolver = new multigrid_d2(gridData, inputParams);
-#else
-    mgSolver = new multigrid_d3(gridData, inputParams);
-#endif
-
-    mgSolver->mgSolve(Pp, mgRHS);
-
-    // FINALIZE AND CLEAN-UP
-    MPI_Finalize();
-
-    return 0;
 }
